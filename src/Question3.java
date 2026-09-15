dc9593194af51dd04ed16ec9daebdae1704723b0
fkdsip004-hub <fkdsip004@myuct.ac.za>
2026-09-15 16:29:37 +0200

    Initial commit

---
+++ src/Question3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+class Question3
+{
+   public static void main ( String args [] )
+   {
+      System.out.println (new Person ("Thabo Lipholo", "0808295432080"));
+      System.out.println (new Student ("Lesedi Lebaka", "0208195432088",  "LBKLES001", "BSc Eng (Civil Engineering)"));
+      System.out.println (new StaffMember ("Lebeko Poulo", "7909296542188", "01421403", "Computer Science"));
+   }
+}
